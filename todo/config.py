d804d3f9bc62d7da6b254a7249370aa97be74ca8
Filip <filip.uradnik9@gmail.com>
2023-02-18 19:41:16 +0100

    Implement source_from_dict func.

--- todo/config.py
+++ todo/config.py
@@ -10,16 +10,16 @@ SOURCES_CONFIG_NAMES = {
 }
 
 
+def get_source_from_dict(conf: dict) -> Source:
+    """Get Source object from config."""
+    return SOURCES_CONFIG_NAMES[conf["source"]["name"]](
+        **(conf["source"]["params"] if "params" in conf["source"].keys() else {})
+    )
+
+
 def get_source_from_config(path: Path = CONFIG_FILE_PATH) -> Source:
     """Get config from file."""
     with path.open() as f:
         conf = load(f)
 
-    source = SOURCES_CONFIG_NAMES[conf["source"]["name"]](
-        **(conf["source"]["params"] if "params" in conf["source"].keys() else {})
-    )
-    return source
-
-
-def get_source_from_dict(conf: dict) -> Source:
-    """TODO: implement later."""
+    return get_source_from_dict(conf)
