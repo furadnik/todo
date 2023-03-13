beaedd1299d243c7404ea55d9f8f94286aea2d56
Filip <filip.uradnik9@gmail.com>
2023-03-13 21:54:44 +0100

    Add tests.

---
+++ todo/tests/test_client.py
@@ -0,0 +1,29 @@
+from todo.client import TodoList, Task
+from functools import partial
+from unittest import TestCase
+from unittest.mock import Mock
+
+
+class TestTodoList(TestCase):
+
+    def setUp(self):
+        self.source = Mock()
+        self.todo_list = TodoList(self.source)
+        self.source.fetch = lambda: map(partial(Task, self.todo_list), ["a", "b", "c"])
+
+    def test_iterator(self):
+        self.assertEqual(list(self.todo_list), ["a", "b", "c"])
+
+    def test_contains(self):
+        self.assertTrue("a" in self.todo_list)
+
+    def test_contains_tasks(self):
+        for task in self.todo_list:
+            self.assertTrue(task in self.todo_list)
+
+    def test_not_contains(self):
+        self.assertFalse("d" in self.todo_list)
+
+    def test_finding(self):
+        self.assertIsNone(self.todo_list.find_task_by_title("task"))
+        self.assertIsNotNone(self.todo_list.find_task_by_title("a"))
