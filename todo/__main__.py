eccf4d27f08bf524ff43feec45825b10b9e0b4fc
Filip <filip.uradnik9@gmail.com>
2023-08-22 20:28:39 +0200

    Add `main` function to entrypoint.

--- todo/__main__.py
+++ todo/__main__.py
@@ -4,23 +4,30 @@ from argparse import ArgumentParser
 from .client import TodoList
 from .config import get_source_from_config
 
-source = get_source_from_config()
-todo = TodoList(source)
 
-ap = ArgumentParser()
-sp = ap.add_subparsers()
+def main() -> None:
+    """Run main function."""
+    source = get_source_from_config()
+    todo = TodoList(source)
 
-todo_list = sp.add_parser("list", aliases=["l", "get", "g"], help="list tasks.")
-todo_list.set_defaults(func=lambda _, td: [print(x.title) for x in td.get_tasks()])
+    ap = ArgumentParser()
+    sp = ap.add_subparsers()
 
-todo_remo = sp.add_parser("remo", aliases=["r", "d", "delete"], help="delete task.")
-todo_remo.add_argument("task", type=str, help="task title.")
-todo_remo.set_defaults(func=lambda args, td: td.remove_task(args.task))
+    todo_list = sp.add_parser("list", aliases=["l", "get", "g"], help="list tasks.")
+    todo_list.set_defaults(func=lambda _, td: [print(x.title) for x in td.get_tasks()])
 
-todo_add = sp.add_parser("add", aliases=["a", "c", "create"], help="create task.")
-todo_add.add_argument("task", type=str, help="task title.")
-todo_add.set_defaults(func=lambda args, td: td.add_task(args.task))
+    todo_remo = sp.add_parser("remo", aliases=["r", "d", "delete"], help="delete task.")
+    todo_remo.add_argument("task", type=str, help="task title.")
+    todo_remo.set_defaults(func=lambda args, td: td.remove_task(args.task))
 
-args = ap.parse_args()
+    todo_add = sp.add_parser("add", aliases=["a", "c", "create"], help="create task.")
+    todo_add.add_argument("task", type=str, help="task title.")
+    todo_add.set_defaults(func=lambda args, td: td.add_task(args.task))
 
-args.func(args, todo)
+    args = ap.parse_args()
+
+    args.func(args, todo)
+
+
+if __name__ == '__main__':
+    main()
